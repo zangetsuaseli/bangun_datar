14e22c6556702193d8a6c64f2cd67ec7bdba6be6
USER <abdullahazam045@gmail.com>
2023-11-23 09:38:56 +0700

    custom component

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bangun_datar_kelas_c/page/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,1 +1,39 @@
 import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Home Page",
+          style: TextStyle(color: Color(0xFFFFFFFF)),
+        ),
+        backgroundColor: colors.blue.shade300,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: CustomMenu(title: "Persegi", imageAsset: "assets/img.png"))
+                Expanded(child: CustomMenu(title: "Persegi", imageAsset: "assets/img.png"))
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: CustomMenu(title: "Persegi", imageAsset: "assets/img.png"))
+                Expanded(child: CustomMenu(title: "Persegi", imageAsset: "assets/img.png"))
+              ],
+            ),
+          )
+        ]
+      ),
+    );
+  },
+},
